3070d26868b51f919d3da2632b010adaeffe8d0c
Lin Bai <linbai023@gmail.com>
2018-09-22 11:59:47 -0400

    09/22-01

--- sqlimplement.py
+++ sqlimplement.py
@@ -2,14 +2,74 @@ import pymysql.cursors
 import pandas as pd
 import json
 
-cnx= pymysql.connect(host='localhost',
-                     user='root',
-                     password='Bailin960203',
-                     port=3306,
-                     db='lenman2017',
-                     charset='utf8mb4',
-                     cursorclass=pymysql.cursors.DictCursor)
+class RDBDataTable:
+    def __init__(self, t_name, t_file, key_columns):
+        self.t_name = t_name
+        self.t_file = t_file
+        self.key_columns = key_columns
 
+
+        self.cnx= pymysql.connect(host='localhost',
+                                     user='root',
+                                     password='Bailin960203',
+                                     port=3306,
+                                     db='lenman2017',
+                                     charset='utf8mb4',
+                                     cursorclass=pymysql.cursors.DictCursor)
+
+
+    def template_to_where_clause(self, t):
+        s = ""
+        for (k, v) in t.items():
+            if s != "":
+                s += " AND "
+            s += k + "='" + v + "'"
+
+        if s != "":
+            s = "WHERE " + s;
+
+        return s
+
+    def find_by_primary_key (self, key_values, values=None):
+        res=[]
+        for i in range(len(key_values)):
+            template = dict(zip(self.key_columns, key_values[i]))
+            result = self.find_by_template(template, values)
+            if len(result) > 0:
+                res.append(result[0])
+        return res
+
+
+    def delete(self, template):
+        w = self.template_to_where_clause(template)
+        q = "delete from " + self.t_file + " " + w
+        print("q = ", q)
+        cursor = self.cnx.cursor()
+        cursor.execute(q)
+        q2 = "select row_count() as count"
+        cursor.execute(q2)
+        f = cursor.fetchall()
+        print("DB delete", f)
+        self.cnx.commit()
+
+    def find_by_template(self, t, fields=None):
+        if fields is None:
+            fields = ['*']
+
+        w = self.template_to_where_clause(t)
+        cursor = self.cnx.cursor()
+        q = "SELECT " + ",".join(fields) + " FROM " + self.t_file + " " + w + ";"
+        print("Query = ", q)
+        cursor.execute(q);
+        r = cursor.fetchall()
+        #print("Query result = ", r)
+        return r
+
+#def insert(self, row):
+
+
+
+'''
 def DBRunQuery(q):
     cursor=cnx.cursor()
     print ("Query = ", q)
@@ -17,7 +77,10 @@ def DBRunQuery(q):
     r = cursor.fetchall()
     #print("Query result = ", r)
     return r
+    '''
 
-result = DBRunQuery("select * from people where playerid='willite01'")
-print("The result is of type: ", list(result))
-print("OK. The answer is an instance of the class 'tuple.'")
+
+
+rdt = RDBDataTable("Foo", "Batting", ['playerID', 'teamID'])
+result = rdt.find_by_primary_key([['willite01', 'BOS'],['allisar01','CL1']])
+print("Result = ", json.dumps(result, indent=3))
